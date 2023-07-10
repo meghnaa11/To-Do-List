5b1385fef24fe2b2cb3f3178379eda31a446e322
Meghna Agrawal <meghnaagrawal10@gmail.com>
2023-07-10 22:42:48 +0530

    replacing existing list item

--- app.js
+++ app.js
@@ -4,39 +4,28 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+var item = "";
 
 app.get("/", function (req, res) {
   var today = new Date();
-  var day = "";
 
-  switch (today.getDay()) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default: 
-      console.log("Error: current day is: " + today.getDay());
-  }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: day, newListItem: item });
 });
+
+// if we render inside post, we will get error bc newListItem will be undefined during the first get method so we do it with the kindOfDay
+app.post("/", function(req, res) {
+  item = req.body.newItem;
+  res.redirect("/");
+})
 
 app.listen(3000, function () {
   console.log("Server started on port 3000.");
