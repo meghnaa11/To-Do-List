19767229670b58ba4f2a81626447b09d7f904f08
Meghna Agrawal <meghnaagrawal10@gmail.com>
2023-07-12 12:05:05 +0530

    added another Work list page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 var items = [];
+var workItems = [];
 
 app.get("/", function (req, res) {
   var today = new Date();
@@ -19,15 +20,31 @@ app.get("/", function (req, res) {
   };
   var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 // if we render inside post, we will get error bc newListItem will be undefined during the first get method so we do it with the kindOfDay
 app.post("/", function(req, res) {
   var item = req.body.newItem;
+  if(req.body.button === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+  
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", { listTitle: "Work Items", newListItems: workItems });
+});
+
+app.post("/work", function(req, res) {
+  var item = req.body.newItem;
   items.push(item);
-  res.redirect("/");
-})
+  res.redirect("/work");
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000.");
